a6cea6a0096742a1597d298f7220fbc0d4930b90
unknown <mailzahidul@gmail.com>
2023-07-25 00:07:26 +0600

    data save to database

--- ami_coding_pari_na/main_app/views.py
+++ ami_coding_pari_na/main_app/views.py
@@ -1,36 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from user_admin.models import User
+from datetime import datetime
+from .models import InputValue
 from django.contrib import messages
-from user_admin.models import User as UUser
 # Create your views here.
 
 @login_required
 def home(request):
     context={}
+    datetime_format = '%d-%m-%Y %H:%M:%S'
+    login_user = request.user
+    user_id = User.objects.get(email=login_user)
+    if request.method == 'POST':
+        timestamp = datetime.now().strftime(datetime_format)
+        input_values = request.POST['input_values']
+        InputValue.objects.create(user=user_id, timestamp=timestamp, input_values=input_values)
+
     return render(request, 'index.html', context)
-
-
-@login_required
-def quiz_list(request):
-    context={}
-    return render(request, 'main_app/quiz_list.html', context)
-
-@login_required
-def quiz_test(request, id):
-   context={}
-
-
-   return render(request, 'main_app/quiz_test.html', context)
-
-
-
-
-def add_quiz(request):
-    context={}
-    return render(request, 'main_app/add_quiz.html', context)
-
-
-def resutl_view(request):
-    context={}
-    return render(request, 'main_app/result.html', context)
